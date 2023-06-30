6c3a0290f2a4f77138acc1c03c857aae9abe5aca
yutteee <yusaku_nakamura_aa@icloud.com>
2023-06-30 15:43:08 +0900

    styled-component導入 #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Link href="/projects">start</Link>
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
